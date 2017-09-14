54889167f59d0b25bb24f12a30f9c6666cdfe7d4
15cs026priyanka <priyankaappu1998@gmail.com>
2017-09-14 14:45:47 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int base,exponat;
+clrscr();
+printf("enter the base number");
+scanf("%d",&n);
+printf("enter the exponat number");
+scanf("%d",&n);
+while(base!=0)
+{
+printf("answer=%d",answer);
+getch();
+}
